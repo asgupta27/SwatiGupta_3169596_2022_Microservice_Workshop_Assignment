0d4ae7c6f8495f8a0972bb0cb197e3e8ee25e649
Swati Gupta <swati.gupta01@nagarro.com>
2022-04-12 08:24:46 +0530

    assignment-proj

---
+++ SwatiGupta_3169596_2022_Microservice_Workshop_Assignment/Microservices/ConsumerAPI/Entities/Consumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI.Entities
+{
+    public class Consumer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public int Location { get; set; }
+        public string EmailId { get; set; }
+    }
+}
---
+++ SwatiGupta_3169596_2022_Microservice_Workshop_Assignment/Microservices/ConsumerAPI/Repositories/ConsumerRepository.cs
@@ -0,0 +1,34 @@
+﻿using ConsumerAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI.Repositories
+{
+    public class ConsumerRepository
+    {
+        private readonly List<Consumer> consumers;
+        public ConsumerRepository()
+        {
+            consumers = new List<Consumer>
+            {
+                new Consumer { Id = 1, Name = "Test Consumer", Username = "Consumer1", EmailId = "Consumer1@test.com" },
+                new Consumer { Id = 2, Name = "Sample Consumer", Username = "Consumer2", EmailId = "Consumer2@test.com" },
+                new Consumer { Id = 3, Name = "Test Consumer2", Username = "Consumer3", EmailId = "Consumer3@test.com" },
+                new Consumer { Id = 4, Name = "Test Consumer3", Username = "Consumer4", EmailId = "Consumer4@test.com" },
+                new Consumer { Id = 5, Name = "Test Consumer4", Username = "Consumer5", EmailId = "Consumer5@test.com" },
+            };
+        }
+
+        public List<Consumer> GetConsumers()
+        {
+            return consumers;
+        }
+
+        public Consumer GetConsumerById(int id)
+        {
+            return consumers.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
