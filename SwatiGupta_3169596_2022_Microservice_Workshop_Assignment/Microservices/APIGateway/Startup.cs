352d8c90863ad374a63ab42bdc897466f26a43c7
Swati Gupta <swati2010gupta@gmail.com>
2022-04-13 10:25:31 +0530

    latest code

--- SwatiGupta_3169596_2022_Microservice_Workshop_Assignment/Microservices/APIGateway/Startup.cs
+++ SwatiGupta_3169596_2022_Microservice_Workshop_Assignment/Microservices/APIGateway/Startup.cs
@@ -3,10 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace APIGateway
 {
@@ -16,6 +15,7 @@ namespace APIGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +35,8 @@ namespace APIGateway
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            app.UseOcelot();
         }
     }
 }
